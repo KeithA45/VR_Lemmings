60218018dc950c0932976a836a51306d236ddf66
Keith Ayers <KeitAyer@microsoft.com>
2022-09-22 15:18:11 -0700

    Overhaul on Lemming and spawn script

--- Assets/Scripts/SpawnLemmings.cs
+++ Assets/Scripts/SpawnLemmings.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class SpawnLemmings : MonoBehaviour
 {
     public GameObject objectToSpawn;
+    public float initialDelayInSeconds = 10f;
+    public float repeatedDelayInSeconds = 10f;
+
+    public float spawnHeightAdjustment = 0.35f;
+    public Vector3 spawnRangeVariation = new Vector3(0.5f, 0f, 0.5f);
+
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnObject", 2, 1);
+        InvokeRepeating("SpawnObject", initialDelayInSeconds, repeatedDelayInSeconds);
     }
 
     // Update is called once per frame
@@ -19,17 +25,16 @@ public class SpawnLemmings : MonoBehaviour
 
     Vector3 GetSpawnPoint() 
     {
-        // Values are currently hard coded to positions on start platform
-        float x = Random.Range(-0.9f, 1.83f);
-        float y = Random.Range(0.97f, 1);
-        float z = Random.Range(-5.3f, -2.4f);
+        float x = Random.Range(0 - spawnRangeVariation.x, spawnRangeVariation.x);
+        float y = Random.Range(0 - spawnRangeVariation.y, spawnRangeVariation.y);
+        float z = Random.Range(0 - spawnRangeVariation.z, spawnRangeVariation.z);
 
-        return new Vector3(x, y, z);
+        return transform.position + new Vector3(x, y + spawnHeightAdjustment, z);
     }
 
     void SpawnObject()
     {
-        // NOTE: Quaterion is the rotation
-        Instantiate(objectToSpawn, GetSpawnPoint(), Quaternion.identity);
+        // use the platform's rotation to determine which way the lemmings should go
+        Instantiate(objectToSpawn, GetSpawnPoint(), transform.rotation);
     }
 }
